fdce469289535fb42b98fdedd2763999e51eccd6
vinudeep <vinudeep.malalur@o2.com>
2017-07-26 00:14:41 +0530

    Change-Id: I46514b15b522f2d1118776c88f665e67e5d7cb1a Signed-off-by: vinudeep <vinudeep.malalur@o2.com>

--- src/test/java/steps/E2EOrderPlaced_Steps.java
+++ src/test/java/steps/E2EOrderPlaced_Steps.java
@@ -3806,5 +3806,23 @@ public class E2EOrderPlaced_Steps {
 			Assert.fail("Unable to Validate basecomms offer link");
 		}
 	}
+	@Then("^check if the selected non connected device has more than (\\d+) variant for colour and single variant for capacity$")
+	public void check_if_the_selected_non_connected_device_has_more_than_variant_for_colour_and_single_variant_for_capacity(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	}
+	
+	@Given("^I choose upgrade PayM Iphone(\\d+)$")
+	public void i_choose_upgrade_PayM_Iphone(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	}
+	
+	@Given("^I choose upgrade PayM tablet Apple iPad Pro (\\d+)\\.(\\d+) inch$")
+	public void i_choose_upgrade_PayM_tablet_Apple_iPad_Pro_inch(int arg1, int arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	}
 
+	@Then("^check if the selected device has only (\\d+) variant for both colour and capacity$")
+	public void check_if_the_selected_device_has_only_variant_for_both_colour_and_capacity(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	}
 }
