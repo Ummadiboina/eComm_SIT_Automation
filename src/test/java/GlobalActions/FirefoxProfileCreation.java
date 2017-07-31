5b74dfcaef9c2bc887590b684b2392d60d1da04a
vinudeep <vinudeep.malalur@o2.com>
2017-07-31 16:07:18 +0530

    Change-Id: I6b6231bfee99981d3296eb4bb25929821421b395 Signed-off-by: vinudeep <vinudeep.malalur@o2.com>

---
+++ src/test/java/GlobalActions/FirefoxProfileCreation.java
@@ -0,0 +1,26 @@
+package GlobalActions;
+
+import org.apache.log4j.Logger;
+
+import helpers.Environment;
+
+public class FirefoxProfileCreation extends Environment  {
+
+	static Logger log = Logger.getLogger("devpinoyLogger");
+
+		public static void openFirefoxProfile() throws InterruptedException 
+		{
+	System.out.println("executing batch run");
+		     String filePath = "C:/batchfileForSelenium/openFirefoxProfile.bat";
+	         try {
+	              
+	             Process p = Runtime.getRuntime().exec(filePath);
+	             
+	         } catch (Exception e) {
+	             e.printStackTrace();
+	         }
+	     }
+	  		  	  
+
+	}
+
