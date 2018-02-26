a75ab44ebb8e0b5f6806e5fa5f145e75cd031c4b
SNarasi1 <sharmendiran.narasimhan@o2.com>
2018-02-26 17:56:55 +0000

    sharmi 26/02 changes
    Signed-off-by: SNarasi1 <sharmendiran.narasimhan@o2.com>

--- src/test/java/steps/E2EOrderPlaced_Steps.java
+++ src/test/java/steps/E2EOrderPlaced_Steps.java
@@ -8363,15 +8363,6 @@ public class E2EOrderPlaced_Steps {
     }
 
 
-    @Given("^Read the data from exel with the location ([^\"]*)$")
-    public void readTestDataFromExel(String exelLocation) {
-        try {
-            System.out.println("Reading the data From Exel sheet: for validating the Test data :");
-            ReadData.readingDataFromExel(exelLocation);
-        } catch (Exception e) {
-            System.out.println("Unable to validate section");
-            Assert.fail("Unable to validate section");
-        }
-    }
+
 
 }
