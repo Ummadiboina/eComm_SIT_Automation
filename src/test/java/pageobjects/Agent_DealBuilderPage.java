3a4654fab9e2a1dfce8a61f3fe035b68e1cf12d5
sharmi-o2-bitbucket <sharmi30@gmail.com>
2019-04-26 15:17:09 +0000

    Agent_DealBuilderPage.java edited online with Bitbucket

--- src/test/java/pageobjects/Agent_DealBuilderPage.java
+++ src/test/java/pageobjects/Agent_DealBuilderPage.java
@@ -63,7 +63,7 @@ public class Agent_DealBuilderPage {
     @FindBy(how = How.XPATH, using = "//*[@id='planTable']/tbody/tr[2]/td[1]")
     public static WebElement SelectingFirstAvailableTariff;
 
-    @FindBy(how = How.XPATH, using = "//*[@id='prepayPlanTable']/tbody/tr[1]/td[1]")
+    @FindBy(how = How.XPATH, using = "//*[@id='prepayPlanTable']/tbody/tr[2]/td[1]")
     public static WebElement SelectingFirstAvailablePrePayTariff;
 
     @FindBy(how = How.XPATH, using = "//table[@id='prepayDeviceTable']/tbody/tr/td/a/img")
@@ -418,27 +418,13 @@ public class Agent_DealBuilderPage {
     @FindBy(how = How.XPATH, using = ("//*[text()='Email sent successfully']"))
     public static WebElement emailSuccessfullyMsg;
 
+    @FindBy(how = How.XPATH, using = ("//a[@id='simoPlanTab']"))
+    public static WebElement SIMOTariffsTab;
 
-    //Arizona starts here
-    @FindBy(how = How.ID, using = ("devicePairingContinue"))
-    public static WebElement leadDevicePairingSection_Continue;
+    @FindBy(how = How.XPATH, using = ("//div[@id='simoPlanTabContent']/div/div/label/input"))
+    public static WebElement SIMOTariffsInputField;
 
-    @FindBy(how = How.XPATH, using = ("//*[@id=\"noEligibleMsisdn\"]"))
-    public static WebElement leadDevicePairingSection_Error;
-
-    @FindBy(how = How.XPATH, using = ("//*[@id=\"leadDevicePairing\"]/h2"))
-    public static WebElement leadDevicePairingSection_Heading;
-
-    @FindBy(how = How.XPATH, using = ("//*[@id=\"leadDeviceMsisdn\"]"))
-    public static WebElement leadDevicePairingSection_dropdown;
-
-    @FindBy(how = How.XPATH, using = ("//*[@id='leadDevicePairingLabel']"))
-    public static WebElement leadDevicePairingSection_label;
-
-    @FindBy(how = How.XPATH, using = ("//*[@id='sharedBasketContents']/div/div[2]/table[4]"))
-    public static WebElement leadDeviceMSISDNSection_ordersummary;
-
-    @FindBy(how = How.XPATH, using = ("(//*[@id='_'])[4]"))
-    public static WebElement leadDeviceMSISDN_ordersummary;
+    @FindBy(how = How.XPATH, using = ("//*[@id='simonlyPlanTable']/tbody/tr[1]/td[1]"))
+    public static WebElement SelectingFirstAvailableSIMOTariff;
 
 }
