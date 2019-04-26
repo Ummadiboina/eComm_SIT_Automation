46b9141922bdd895b63d32679aa44f2ef2fa77af
sharmi-o2-bitbucket <sharmi30@gmail.com>
2019-04-26 15:09:52 +0000

    RunnerTest.java edited online with Bitbucket

--- src/test/java/testRunner/RunnerTest.java
+++ src/test/java/testRunner/RunnerTest.java
@@ -39,7 +39,7 @@ import java.util.List;
 @CucumberOptions(plugin = {"html:testResult/cucumber-html-report",
         "json:testResult/cucumber.json", "pretty:testResult/cucumber-pretty.txt",
         "usage:testResult/cucumber-usage.json", "junit:testResult/cucumber-results.xml"},
-        features = {"Features//MayRelease2019//ITFD-895//FirstBatchRun"},
+        features = {"Features//AprilRelease2019//CFA_Reg_FailedScenarios"},
         glue = {"steps"},
         tags = {"@Web"})
 
